1e538a016728728cac95be63f6786ca0b280d716
kshubham506 <kshubham506@gmail.com>
2019-11-12 18:09:49 +0530

    train,track

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import os
+from PIL import Image, ImageTk
 import cv2
+import numpy as np
 
-def takeimage(name):
-    print("\nTakign images for user : ",name)
+def takeimage(id):
+    print("\nTakign images for user : ",id)
     
     harcascadePath = "haarcascade_frontalface_default.xml"
     detector=cv2.CascadeClassifier(harcascadePath)
@@ -21,19 +24,87 @@ def takeimage(name):
     
             count =count +1
             
-            cv2.imwrite("TrainingImage\ "+name +'.'+ str(count) + ".jpg", gray[y:y+h,x:x+w])
+            cv2.imwrite("TrainingImage\ "+id +'.'+ str(count) + ".jpg", gray[y:y+h,x:x+w])
             #display the frame
             cv2.imshow('frame',frame)
         #wait for 100 miliseconds 
         if cv2.waitKey(100) & 0xFF == ord('q'):
             break
         # break if the sample number is morethan 100
-        elif count>60:
+        elif count>=60:
             break
     cam.release()
     cv2.destroyAllWindows() 
     
-    print("\nImage Taken")
+    print("\nImages Taken")
+    
+    
+def TrainImages():
+    print("Trainign Images")
+    recognizer = cv2.face_LBPHFaceRecognizer.create()
+    #recognizer = cv2.face.LBPHFaceRecognizer_create()#$cv2.createLBPHFaceRecognizer()
+#    harcascadePath = "haarcascade_frontalface_default.xml"
+#    detector =cv2.CascadeClassifier(harcascadePath)
+    
+    faces,Id = getImagesAndLabels("TrainingImage")
+    
+    recognizer.train(faces, np.array(Id))
+    recognizer.save("TrainingImageLabel\Trainner.yml")
+    print("Image Trained")#+",".join(str(f) for f in Id)
+#    message.configure(text= res)
+
+def getImagesAndLabels(path):
+    imagePaths=[os.path.join(path,f) for f in os.listdir(path)] 
+    
+#    print(imagePaths)
+    
+    faces=[]
+    Ids=[]
+    for imagePath in imagePaths:
+        pilImage=Image.open(imagePath).convert('L')
+        imageNp=np.array(pilImage,'uint8')
         
+        abc=os.path.split(imagePath)[-1].split(".")[1]
+#        print("\n",abc)
+        Id=int(abc)
+        faces.append(imageNp)
+        Ids.append(Id)        
+    return faces,Ids
         
-takeimage("shubham")
+def castVote():
+    print("Casting Vote")
+    recognizer = cv2.face.LBPHFaceRecognizer_create()#cv2.createLBPHFaceRecognizer()
+    recognizer.read("TrainingImageLabel\Trainner.yml")
+    harcascadePath = "haarcascade_frontalface_default.xml"
+    faceCascade = cv2.CascadeClassifier(harcascadePath);    
+   
+    cam = cv2.VideoCapture(0)
+      
+       
+    while True:
+        ret, im =cam.read()
+        gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+        faces=faceCascade.detectMultiScale(gray, 1.2,5) 
+        
+        for(x,y,w,h) in faces:
+            cv2.rectangle(im,(x,y),(x+w,y+h),(225,0,0),2)
+            Id, conf = recognizer.predict(gray[y:y+h,x:x+w])
+            print(Id," : ",conf)                                   
+            
+        if conf<40:
+                break 
+            
+        cv2.imshow('im',im)
+        
+        if (cv2.waitKey(1)==ord('q')):
+            break
+        
+ 
+    cam.release()
+    cv2.destroyAllWindows()
+    
+    print("Vote casted")
+     
+#takeimage("shubham")   
+#TrainImages()
+castVote()
